f98ad06a47a27f6bd65fd67b5431300091254226
Hampus Ram <hampus.ram@educ.goteborg.se>
2024-09-25 12:00:26 +0200

    lektion 7 övning

---
+++ lektion7/MathFunctions.java
@@ -0,0 +1,19 @@
+public class MathFunctions {
+    public static double max(double num1, double num2) {
+        if (num1 > num2) {
+            return num1;
+        }
+        else {
+            return num2;
+        }
+    }
+
+    public static int max(int num1, int num2) {
+        if (num1 > num2) {
+            return num1;
+        }
+        else {
+            return num2;
+        }
+    }
+}
---
+++ lektion7/MathMain.java
@@ -0,0 +1,6 @@
+public class MathMain {
+    public static void main(String[] args) {
+        double max = MathFunctions.max(23.2, 19.12);
+        System.out.println(max);
+    }
+}
