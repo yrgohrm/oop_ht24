8ea624c53163f0d0c5e654e39a35a0b829143ebc
Hampus Ram <hampus.ram@educ.goteborg.se>
2024-11-26 10:17:19 +0100

    fixat indentering

--- lektion23/Ex2DataServer.java
+++ lektion23/Ex2DataServer.java
@@ -4,27 +4,27 @@ import java.util.concurrent.*;
 
 public class Ex2DataServer {
     public static void main(String[] args) throws IOException {
-// NB! This code is lacking a lot of try-with-resources for clarity
-ServerSocket server = new ServerSocket(12345);
+        // NB! This code is lacking a lot of try-with-resources for clarity
+        ServerSocket server = new ServerSocket(12345);
 
-// the program will wait here until someone connects
-Socket connectionToClient = server.accept();
+        // the program will wait here until someone connects
+        Socket connectionToClient = server.accept();
 
-InputStream is = connectionToClient.getInputStream();
-DataInputStream dataIn = new DataInputStream(is);
+        InputStream is = connectionToClient.getInputStream();
+        DataInputStream dataIn = new DataInputStream(is);
 
-OutputStream os = connectionToClient.getOutputStream();
-DataOutputStream dataOut = new DataOutputStream(os);
+        OutputStream os = connectionToClient.getOutputStream();
+        DataOutputStream dataOut = new DataOutputStream(os);
 
-int random;
-do {
-    random = dataIn.readInt();
-    System.out.println("Request: " + random);
-    dataOut.writeInt(ThreadLocalRandom.current().nextInt(20));
-    dataOut.flush();
-} while (random != 0);
+        int random;
+        do {
+            random = dataIn.readInt();
+            System.out.println("Request: " + random);
+            dataOut.writeInt(ThreadLocalRandom.current().nextInt(20));
+            dataOut.flush();
+        } while (random != 0);
 
-connectionToClient.close();
-server.close();
+        connectionToClient.close();
+        server.close();
     }
 }
